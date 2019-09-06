5f11fdb72fa7d99681f860d64a78cbd3566b0ba1
TuVanPhong <phongken94@gmail.com>
2019-09-06 23:07:56 +0700

    tailen

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,11 @@
+
+    public class MyClass{
+        public static void main(String[] args){
+            int i=2;
+            for( ; i <= 5; i++){
+                System.out.println("Hello");
+            }
+        }
+
+    }
+
